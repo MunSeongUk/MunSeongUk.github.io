f489bd6ec7310e7bfd606548e0f50b60bf545793
MunSeongUk <door6740@gmail.com>
2020-06-01 12:12:14 +0900

    안쓰는 파일 제거

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,33 @@
-$(document).ready(function(){$(".owl-carousel").owlCarousel({loop:!0,margin:30,autoplay:!0,autoplayHoverPause:!0,slideBy:3,mouseDrag:!1,dotsContainer:".owl-dots",responsive:{0:{items:1},768:{items:2},992:{items:2},1200:{items:3}}})});
+$(function(){
+    $('#nationalassembly-hack-video').modal({
+        show: false
+    }).on('hidden.bs.modal', function() {
+        $('#nationalassembly-hack-video div div div.plyr div button')[0].click();
+    }).on('show.bs.modal', function() {
+        $('#nationalassembly-hack-video div div div.plyr div button')[0].click();
+    });
+
+    $('#korea-china-hack-video').modal({
+        show: false
+    }).on('hidden.bs.modal', function() {
+        $('#korea-china-hack-video div div div.plyr div button')[0].click();
+    }).on('show.bs.modal', function() {
+        $('#korea-china-hack-video div div div.plyr div button')[0].click();
+    });
+
+    $('#angelhack-video').modal({
+        show: false
+    }).on('hidden.bs.modal', function() {
+        $('#angelhack-video div div div.plyr div button')[0].click();
+    }).on('show.bs.modal', function() {
+        $('#angelhack-video div div div.plyr div button')[0].click();
+    });
+
+    $('#bizcon-contest2-video').modal({
+        show: false
+    }).on('hidden.bs.modal', function() {
+        $('#bizcon-contest2-video div div div.plyr div button')[0].click();
+    }).on('show.bs.modal', function() {
+        $('#bizcon-contest2-video div div div.plyr div button')[0].click();
+    });
+});
